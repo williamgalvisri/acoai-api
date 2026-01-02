71a45e5e12e2ddc76ce20137d57eb651b49fcb98
William Galvis <williamgalvisrivera@gmail.com>
2026-01-02 18:35:53 -0500

    fix: Correct auth middleware import path.

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 
 router.use(protect);
